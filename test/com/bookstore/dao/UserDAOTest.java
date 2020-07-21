01c1af20408b866a44fd6b2f45a7731f339ab553
Mohammed Rahman <minhaz357@gmail.com>
2020-07-21 12:35:12 -0400

    Add setUp() and tearDown() methods
    	- organize the code
    	- use @BeforeClass and @AfterClass annotations
    	- make EntityManagerFactory and EntityManager variables as class variables

--- test/com/bookstore/dao/UserDAOTest.java
+++ test/com/bookstore/dao/UserDAOTest.java
@@ -6,36 +6,47 @@ import javax.persistence.EntityManager;
 import javax.persistence.EntityManagerFactory;
 import javax.persistence.Persistence;
 
+import org.junit.AfterClass;
+import org.junit.BeforeClass;
 import org.junit.Test;
 
 import com.bookstore.entity.Users;
 
 public class UserDAOTest {
+	private static EntityManagerFactory entityManagerFactory;
+	private static EntityManager entityManager;
+	private static UserDAO userDAO;
+	
+	@BeforeClass
+	public static void setUp() {
+		// create a EntityManager instance from EntityManagerFactory
+		entityManagerFactory = Persistence.createEntityManagerFactory("BookStoreWebsite");
+		entityManager = entityManagerFactory.createEntityManager();
+
+		// create a user into the database
+		userDAO = new UserDAO(entityManager);
+	}
 
 	@Test
 	public void testCreateUsers() {
 		// create Users instance
 		Users user = new Users();
 		Users returnedUser;
-		UserDAO userDAO;
 
-		user.setEmail("abc@example.com");
-		user.setFullName("Kyle Walker");
-		user.setPassword("kylepass");
+		user.setEmail("test3@example.com");
+		user.setFullName("Miranda Wander");
+		user.setPassword("mirandapass");
 
-		// create a EntityManager instance from EntityManagerFactory
-		EntityManagerFactory entityManagerFactory = Persistence.createEntityManagerFactory("BookStoreWebsite");
-		EntityManager entityManager = entityManagerFactory.createEntityManager();
-
-		// create a user into the database
-		userDAO = new UserDAO(entityManager);
 		returnedUser = userDAO.create(user);
-
+		
+		assertTrue(returnedUser.getUserId()> 0);
+	}
+	
+	@AfterClass
+	public static void tearDown() {
 		// close EntityManager and EntityManagerFactory instances
 		entityManager.close();
 		entityManagerFactory.close();
-		
-		assertTrue(returnedUser.getUserId() > 0);
 	}
 
 }
