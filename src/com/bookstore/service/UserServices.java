557a64e2e40bb3010df49146d8835787e3c4939f
Mohammed Rahman <minhaz357@gmail.com>
2020-07-23 16:42:05 -0400

    Add `updateUser` method to update a user

--- src/com/bookstore/service/UserServices.java
+++ src/com/bookstore/service/UserServices.java
@@ -67,16 +67,47 @@ public class UserServices {
 		}
 
 	}
-	
+
 	public void editUser() throws ServletException, IOException {
-		//get id from the request URL
+		// get id from the request URL
 		int userId = Integer.parseInt(request.getParameter("id"));
 		Users user = userDAO.get(userId);
-		
+
 		String editPage = "user_form.jsp";
 		request.setAttribute("user", user);
 		RequestDispatcher requestDispatcher = request.getRequestDispatcher(editPage);
 		requestDispatcher.forward(request, response);
+	}
+
+	public void updateUser() throws ServletException, IOException {
+		// retrieve user information from the request
+		int userId = Integer.parseInt(request.getParameter("userId"));
+		String email = request.getParameter("email");
+		String fullName = request.getParameter("fullname");
+		String password = request.getParameter("password");
+
+		// if there is already another user with the same email address
+		// then don't update the user and forward the request to message.jsp page
+		// and show the error message
+		Users userById = userDAO.get(userId);
+		Users userByEmail = userDAO.findByEmail(email);
+		if (userByEmail != null && userByEmail.getUserId() != userById.getUserId()) {
+			
+			String message = "Could not update! user with " + email + " already exists!";
+			request.setAttribute("message", message);
+			
+			RequestDispatcher requestDispatcher = request.getRequestDispatcher("message.jsp");
+			requestDispatcher.forward(request, response);
+		}else {
+			
+			// create the user that will be updated
+			Users userToBeUpdated = new Users(userId, email, fullName, password);
+			userDAO.update(userToBeUpdated);
+
+			String message = "User has been updated successfully!";
+			listUser(message);
+			
+		}
 	}
 
 }
