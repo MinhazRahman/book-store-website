717587709e67d3870e1fd097c7ad0f486fe54de8
Mohammed Rahman <minhaz357@gmail.com>
2020-07-23 21:30:59 -0400

    Create CategoryDAO.class

---
+++ src/com/bookstore/dao/CategoryDAO.java
@@ -0,0 +1,51 @@
+package com.bookstore.dao;
+
+import java.util.List;
+
+import javax.persistence.EntityManager;
+
+import com.bookstore.entity.Category;
+
+public class CategoryDAO extends JpaDAO<Category> implements GenericDAO<Category>{
+
+	public CategoryDAO(EntityManager entityManager) {
+		super(entityManager);
+		
+	}
+	
+	@Override
+	public Category create(Category category) {
+		return super.create(category);
+	}
+	
+	@Override
+	public Category update(Category category) {
+		
+		return null;
+	}
+
+	@Override
+	public Category get(Object id) {
+		
+		return null;
+	}
+
+	@Override
+	public void delete(Object id) {
+		
+		
+	}
+
+	@Override
+	public List<Category> listAll() {
+		
+		return null;
+	}
+
+	@Override
+	public long count() {
+		
+		return 0;
+	}
+
+}
