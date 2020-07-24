aee5f97d8430a9d420c8578a4d018c2d1633678e
Mohammed Rahman <minhaz357@gmail.com>
2020-07-24 12:22:35 -0400

    Move all Users related servlets to a new package
    	- old package: src.com.bookstore.controller.admin
    	- new package: src.com.bookstore.controller.admin.users

---
+++ src/com/bookstore/controller/admin/users/DeleteUserServlet.java
@@ -0,0 +1,33 @@
+package com.bookstore.controller.admin.users;
+
+import java.io.IOException;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import com.bookstore.service.UserServices;
+
+/*DeleteUserServlet is called when we want to delete a user
+ * on confirmation.
+ * doGet method calls the deleteUser method of UserServices class
+ * 
+ * */
+@WebServlet("/admin/delete_user")
+public class DeleteUserServlet extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+       
+  
+    public DeleteUserServlet() {
+        super();
+    }
+
+	
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		UserServices userServices = new UserServices(request, response);
+		userServices.deleteUser();
+	}
+
+
+}
