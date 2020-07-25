d8ba11d70bedbefd9e46646d2c99d26de1214445
Mohammed Rahman <minhaz357@gmail.com>
2020-07-25 16:43:57 -0400

    Create createCategory() method to create a new category

--- src/com/bookstore/service/CategoryServices.java
+++ src/com/bookstore/service/CategoryServices.java
@@ -4,8 +4,6 @@ import java.io.IOException;
 import java.util.List;
 
 import javax.persistence.EntityManager;
-import javax.persistence.EntityManagerFactory;
-import javax.persistence.Persistence;
 import javax.servlet.RequestDispatcher;
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServletRequest;
@@ -41,8 +39,31 @@ public class CategoryServices extends BaseServices{
 		String listPage = "category_list.jsp";
 		RequestDispatcher requestDispatcher = request.getRequestDispatcher(listPage);
 		
-		// forward the request to the source
+		// forward the request to the category_list.jsp file
 		requestDispatcher.forward(request, response);
+	}
+	
+	public void createCategory() throws ServletException, IOException {
+		
+		// get the parameters from the request object
+		String categoryName = request.getParameter("name");
+		
+		// Check if the Category name already exists in the database.
+		// if the category name is present, then don't create the category
+		Category category = categoryDAO.findByName(categoryName);
+		if(category != null) {
+			String message = "Couldn't create a category! A category with name " + categoryName +
+								" already exists!!";
+			request.setAttribute("message", message);
+			RequestDispatcher requestDispatcher = request.getRequestDispatcher("message.jsp");
+			requestDispatcher.forward(request, response);
+		}else {
+			// Create the category 
+			Category newCategory = new Category(categoryName);
+			categoryDAO.create(newCategory);
+			// show the list of all categories
+			listCategory("New Category has been created successfully!!");
+		}
 	}
 
 }
