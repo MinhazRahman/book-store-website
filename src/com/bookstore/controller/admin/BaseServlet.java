f3ba4a19a75670dc192c3229847397afc133acf5
Mohammed Rahman <minhaz357@gmail.com>
2020-07-25 07:15:50 -0400

    Add BaseServlet class

---
+++ src/com/bookstore/controller/admin/BaseServlet.java
@@ -0,0 +1,32 @@
+package com.bookstore.controller.admin;
+
+import javax.persistence.EntityManager;
+import javax.persistence.EntityManagerFactory;
+import javax.persistence.Persistence;
+import javax.servlet.ServletConfig;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+
+/*In this BaseServlet class we create
+ * EntityManagerFactory and EntityManager objects
+ * and destroy them when the Servlet is no longer exists
+ * */
+
+public abstract class BaseServlet extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+	protected EntityManagerFactory entityManagerFactory;
+	protected EntityManager entityManager;
+
+
+	public void init(ServletConfig config) throws ServletException {
+		entityManagerFactory = Persistence.createEntityManagerFactory("BookStoreWebsite");
+		entityManager = entityManagerFactory.createEntityManager();
+	}
+
+	public void destroy() {
+		entityManager.close();
+		entityManagerFactory.close();
+	}
+
+}
