466b6e61a4e60c4bbb76112510b427f8d6421b1c
Mohammed Rahman <minhaz357@gmail.com>
2020-07-25 18:58:54 -0400

    Create EditCategoryServlet class

---
+++ src/com/bookstore/controller/admin/category/EditCategoryServlet.java
@@ -0,0 +1,31 @@
+package com.bookstore.controller.admin.category;
+
+import java.io.IOException;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import com.bookstore.controller.admin.BaseServlet;
+import com.bookstore.service.CategoryServices;
+
+/**
+ * When we click on the Edit link on category_list.jsp page 
+ * EditCategoryServlet is called. Which in turn calls the 
+ * editCategory() method of CategoryServices class
+ */
+@WebServlet("/admin/edit_category")
+public class EditCategoryServlet extends BaseServlet {
+	private static final long serialVersionUID = 1L;
+       
+    public EditCategoryServlet() {
+        super();
+    }
+
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		CategoryServices categoryServices = new CategoryServices(entityManager, request, response);
+		categoryServices.editCategory();
+	}
+
+}
