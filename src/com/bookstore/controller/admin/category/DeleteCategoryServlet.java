6f8424b83bdc924515eacb8c07317b5e5700b926
Mohammed Rahman <minhaz357@gmail.com>
2020-07-25 21:34:12 -0400

    Create DeleteCategoryServlet class

---
+++ src/com/bookstore/controller/admin/category/DeleteCategoryServlet.java
@@ -0,0 +1,31 @@
+package com.bookstore.controller.admin.category;
+
+import java.io.IOException;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import com.bookstore.controller.admin.BaseServlet;
+import com.bookstore.service.CategoryServices;
+
+/**
+ * DeleteCategoryServlet is called when we click on Ok
+ * button on delete dialogue confirmation box
+ * 
+ */
+@WebServlet("/admin/delete_category")
+public class DeleteCategoryServlet extends BaseServlet {
+	private static final long serialVersionUID = 1L;
+       
+    public DeleteCategoryServlet() {
+        super();
+    }
+
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		CategoryServices categoryServices = new CategoryServices(entityManager, request, response);
+		categoryServices.deleteCategory();
+	}
+
+}
