0e4242e231695d56293d0afb8fb3b6bea5a46769
Mohammed Rahman <minhaz357@gmail.com>
2020-07-26 09:31:06 -0400

    Retrieve all the categories
    	- retrieve all the categories to make those visible
    	on the home page

--- src/com/bookstore/controller/frontend/HomeServlet.java
+++ src/com/bookstore/controller/frontend/HomeServlet.java
@@ -1,6 +1,7 @@
 package com.bookstore.controller.frontend;
 
 import java.io.IOException;
+import java.util.List;
 
 import javax.servlet.RequestDispatcher;
 import javax.servlet.ServletException;
@@ -10,6 +11,8 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
 import com.bookstore.controller.BaseServlet;
+import com.bookstore.dao.CategoryDAO;
+import com.bookstore.entity.Category;
 
 //HomeServlet receives a request, processes it and finally
 //forwards the request to the destination JSP page, index.jsp
@@ -24,6 +27,10 @@ public class HomeServlet extends BaseServlet {
 
 	protected void doGet(HttpServletRequest request, HttpServletResponse response)
 			throws ServletException, IOException {
+		CategoryDAO categoryDAO = new CategoryDAO(entityManager);
+		List<Category> listCategory = categoryDAO.listAll();
+		request.setAttribute("listCategory", listCategory);
+		
 		String homePage = "frontend/index.jsp";
 		RequestDispatcher requestDispatcher = request.getRequestDispatcher(homePage);
 		requestDispatcher.forward(request, response);
