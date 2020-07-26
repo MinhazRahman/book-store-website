dc3daa9da35b1b01c22bf95a5f890cc113d5f5f9
Mohammed Rahman <minhaz357@gmail.com>
2020-07-25 20:17:59 -0400

    Implement updateCategory() method to updata a category

--- src/com/bookstore/service/CategoryServices.java
+++ src/com/bookstore/service/CategoryServices.java
@@ -12,53 +12,51 @@ import javax.servlet.http.HttpServletResponse;
 import com.bookstore.dao.CategoryDAO;
 import com.bookstore.entity.Category;
 
-
-public class CategoryServices extends BaseServices{
+public class CategoryServices extends BaseServices {
 	private CategoryDAO categoryDAO;
 
 	public CategoryServices(EntityManager entityManager, HttpServletRequest request, HttpServletResponse response) {
 		super(entityManager, request, response);
 		categoryDAO = new CategoryDAO(entityManager);
 	}
-	
+
 	public void listCategory() throws ServletException, IOException {
 		listCategory(null);
 	}
-	
+
 	public void listCategory(String message) throws ServletException, IOException {
 		// get the list of all the categories
 		List<Category> listCategory = categoryDAO.listAll();
-		
+
 		// set attributes to the request
 		request.setAttribute("listCategory", listCategory);
-		if(message != null) {
+		if (message != null) {
 			request.setAttribute("message", message);
 		}
-		
+
 		// create request dispatcher
 		String listPage = "category_list.jsp";
 		RequestDispatcher requestDispatcher = request.getRequestDispatcher(listPage);
-		
+
 		// forward the request to the category_list.jsp file
 		requestDispatcher.forward(request, response);
 	}
-	
+
 	public void createCategory() throws ServletException, IOException {
-		
+
 		// get the parameters from the request object
 		String categoryName = request.getParameter("name");
-		
+
 		// Check if the Category name already exists in the database.
 		// if the category name is present, then don't create the category
 		Category category = categoryDAO.findByName(categoryName);
-		if(category != null) {
-			String message = "Couldn't create a category! A category with name " + categoryName +
-								" already exists!!";
+		if (category != null) {
+			String message = "Couldn't create a category! A category with name " + categoryName + " already exists!!";
 			request.setAttribute("message", message);
 			RequestDispatcher requestDispatcher = request.getRequestDispatcher("message.jsp");
 			requestDispatcher.forward(request, response);
-		}else {
-			// Create the category 
+		} else {
+			// Create the category
 			Category newCategory = new Category(categoryName);
 			categoryDAO.create(newCategory);
 			// show the list of all categories
@@ -69,17 +67,42 @@ public class CategoryServices extends BaseServices{
 	public void editCategory() throws ServletException, IOException {
 		// get the parameters from the request
 		int categoryId = Integer.parseInt(request.getParameter("id"));
-		
+
 		// find the category using the id
 		Category category = categoryDAO.get(categoryId);
-		
+
 		// set the category object as the attribute of request object
 		request.setAttribute("category", category);
-		
+
 		// forward the request to the edit page (category_form.jsp file)
 		String editPage = "category_form.jsp";
-		RequestDispatcher  requestDispatcher = request.getRequestDispatcher(editPage);
-		requestDispatcher.forward(request, response);	
+		RequestDispatcher requestDispatcher = request.getRequestDispatcher(editPage);
+		requestDispatcher.forward(request, response);
+	}
+
+	public void updateCategory() throws ServletException, IOException {
+		// get the parameters from the request
+		int categoryId = Integer.parseInt(request.getParameter("categoryId"));
+		String categoryName = request.getParameter("name");
+
+		// find the category by name
+		Category categoryById = categoryDAO.get(categoryId);
+		Category categoryByName = categoryDAO.findByName(categoryName);
+
+		// if the category already exists in the database then
+		// forward the request to the message.jsp file
+		if (categoryByName != null && categoryByName.getCategoryId() != categoryById.getCategoryId()) {
+			String message = "Couldn't create a category! A category with name " + categoryName + " already exists!!";
+			request.setAttribute("message", message);
+			RequestDispatcher requestDispatcher = request.getRequestDispatcher("message.jsp");
+			requestDispatcher.forward(request, response);
+		}else {
+			// if there is no category in the database then
+			// update the category and show the category list
+			Category categoryToBeUpdated = new Category(categoryId, categoryName);
+			categoryDAO.update(categoryToBeUpdated);
+			listCategory("Category has been updated successfully!!");	
+		}
 	}
 
 }
