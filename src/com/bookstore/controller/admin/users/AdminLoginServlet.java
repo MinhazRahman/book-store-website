8e7e9aaba6932e7edf25bbf1b667303a41f95a4f
Mohammed Rahman <minhaz357@gmail.com>
2020-07-30 21:35:50 -0400

    Create AdminLoginServlet to handle the login request

---
+++ src/com/bookstore/controller/admin/users/AdminLoginServlet.java
@@ -0,0 +1,32 @@
+package com.bookstore.controller.admin.users;
+
+import java.io.IOException;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import com.bookstore.controller.BaseServlet;
+import com.bookstore.service.UserServices;
+
+/**
+ * AdminLoginServlet is called when an admin user clicks 
+ * the Login button on the login.jsp page
+ * 
+ * doPost() method calls the login() method of the UserServices class
+ */
+@WebServlet("/admin/login")
+public class AdminLoginServlet extends BaseServlet {
+	private static final long serialVersionUID = 1L;
+       
+    public AdminLoginServlet() {
+        super();
+    }
+
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		UserServices userServices = new UserServices(entityManager, request, response);
+		userServices.login();
+	}
+
+}
