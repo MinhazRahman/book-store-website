3ad0e8c40311756dac4ee6d49d4906cf6b50456b
Mohammed Rahman <minhaz357@gmail.com>
2020-08-04 15:24:16 -0400

    Create showNewBookForm() method

--- src/com/bookstore/service/BookServices.java
+++ src/com/bookstore/service/BookServices.java
@@ -10,14 +10,18 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
 import com.bookstore.dao.BookDAO;
+import com.bookstore.dao.CategoryDAO;
 import com.bookstore.entity.Book;
+import com.bookstore.entity.Category;
 
 public class BookServices extends BaseServices{
 	private BookDAO bookDAO;
+	private CategoryDAO categoryDAO;
 
 	public BookServices(EntityManager entityManager, HttpServletRequest request, HttpServletResponse response) {
 		super(entityManager, request, response);
 		bookDAO = new BookDAO(entityManager);
+		categoryDAO = new CategoryDAO(entityManager);
 	}
 	
 	// retrieves all the books from the database, sets the request attributes
@@ -43,6 +47,18 @@ public class BookServices extends BaseServices{
 	// calls the listBooks(String) method
 	public void listBooks() throws ServletException, IOException {
 		listBooks(null);
+	}
+	
+	public void showNewBookForm() throws ServletException, IOException {
+		List<Category> listCategory = categoryDAO.listAll();
+		
+		// set list of category as attribute
+		request.setAttribute("listCategory", listCategory);
+		
+		// forward the request and response to book_form.jsp page
+		String bookFormPage = "book_form.jsp";
+		RequestDispatcher requestDispatcher = request.getRequestDispatcher(bookFormPage);
+		requestDispatcher.forward(request, response);
 	}
 
 }
