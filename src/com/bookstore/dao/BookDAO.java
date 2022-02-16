8f05556a8c949e49f628e8b7bce6d2f061414849
Mohammed Rahman <minhaz357@gmail.com>
2022-02-16 01:03:40 -0500

    Create searh() method to search books by a keyword

--- src/com/bookstore/dao/BookDAO.java
+++ src/com/bookstore/dao/BookDAO.java
@@ -72,5 +72,11 @@ public class BookDAO extends JpaDAO<Book> implements GenericDAO<Book>{
 		
 		return query.getResultList();
 	}
+	
+	// search for a book by keyword
+	public List<Book> search(String keyword){
+		
+		return super.findWithNamedQuery("Book.search", "keyword", keyword);
+	}
 
 }
