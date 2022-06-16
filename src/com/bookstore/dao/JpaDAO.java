a28df5b9881d13544541149dddc5ed4237516a9c
Mohammed Rahman <minhaz357@gmail.com>
2022-06-16 12:14:53 -0400

    Remove EntityManager and use EntityManagerFactory

--- src/com/bookstore/dao/JpaDAO.java
+++ src/com/bookstore/dao/JpaDAO.java
@@ -6,17 +6,23 @@ import java.util.Map.Entry;
 import java.util.Set;
 
 import javax.persistence.EntityManager;
+import javax.persistence.EntityManagerFactory;
+import javax.persistence.Persistence;
 import javax.persistence.Query;
 
 public class JpaDAO<E> {
-	protected EntityManager entityManager;
+	private static EntityManagerFactory entityManagerFactory;
+	
+	static {
+		entityManagerFactory = Persistence.createEntityManagerFactory("BookStoreWebsite");
+	}
 
-	public JpaDAO(EntityManager entityManager) {
-		super();
-		this.entityManager = entityManager;
+	public JpaDAO() {
+		
 	}
 
 	public E create(E entity) {
+		EntityManager entityManager = entityManagerFactory.createEntityManager();
 		entityManager.getTransaction().begin();
 
 		entityManager.persist(entity);
@@ -24,55 +30,72 @@ public class JpaDAO<E> {
 		entityManager.refresh(entity);
 
 		entityManager.getTransaction().commit();
+		entityManager.close();
 
 		return entity;
 	}
 
 	public E update(E entity) {
+		EntityManager entityManager = entityManagerFactory.createEntityManager();
 		entityManager.getTransaction().begin();
 
 		entity = entityManager.merge(entity);
 		
 		entityManager.getTransaction().commit();
+		entityManager.close();
 
 		return entity;
 	}
 	
 	public E find(Class<E> type, Object id) {
+		EntityManager entityManager = entityManagerFactory.createEntityManager();
 		E entity = entityManager.find(type, id);
 		
 		if(entity != null) {
 			entityManager.refresh(entity);
 		}
 		
+		entityManager.close();
+		
 		return entity;
 	}
 	
 	public void delete(Class<E> type, Object id) {
+		EntityManager entityManager = entityManagerFactory.createEntityManager();
 		entityManager.getTransaction().begin();
 		
 		Object reference = entityManager.getReference(type, id);
 		entityManager.remove(reference);
 		
 		entityManager.getTransaction().commit();
+		entityManager.close();
 	}
 	
 	public List<E> findWithNamedQuery(String queryName){
-		Query query = entityManager.createNamedQuery(queryName);
+		EntityManager entityManager = entityManagerFactory.createEntityManager();
 		
-		return query.getResultList();
+		Query query = entityManager.createNamedQuery(queryName);
+		List<E> result = query.getResultList();
+		entityManager.close();
+		
+		return result;
 	}
 	
 	public List<E> findWithNamedQuery(String queryName, String paramName, Object paramValue){
+		EntityManager entityManager = entityManagerFactory.createEntityManager();
+		
 		Query query = entityManager.createNamedQuery(queryName);
 		query.setParameter(paramName, paramValue);
+		List<E> result = query.getResultList();
+		entityManager.close();
 		
-		return query.getResultList();
+		return result;
 	}
 	
 	// this method takes the name of the query and key/value pairs as parameters
 	// , and return the query result
 	public List<E> findWithNamedQuery(String queryName, Map<String, Object> parameters){
+		EntityManager entityManager = entityManagerFactory.createEntityManager();
 		Query query = entityManager.createNamedQuery(queryName);
 		
 		Set<Entry<String, Object>> setParameters = parameters.entrySet();
@@ -81,13 +104,21 @@ public class JpaDAO<E> {
 			query.setParameter(entry.getKey(), entry.getValue());
 		}
 		
-		return query.getResultList();
+		List<E> result = query.getResultList();
+		entityManager.close();
+		
+		return result;
 	}
 	
 	public long countWithNamedQuery(String queryName) {
+		EntityManager entityManager = entityManagerFactory.createEntityManager();
 		Query query = entityManager.createNamedQuery(queryName);
 		
-		return (long) query.getSingleResult();
+		long result = (long) query.getSingleResult();
+		
+		entityManager.close();
+		
+		return result;
 	}
 
 }
