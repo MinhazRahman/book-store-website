d7f3f00d876a8879d5baf36d1176be7b6b09501f
Mohammed Rahman <minhaz357@gmail.com>
2022-06-16 16:04:07 -0400

    User servlet classes extends HttpServlet
    	- instead of BaseServlet and
    	- remove entityManager parameter

--- src/com/bookstore/controller/admin/users/UpdateUserServlet.java
+++ src/com/bookstore/controller/admin/users/UpdateUserServlet.java
@@ -17,7 +17,7 @@ import com.bookstore.service.UserServices;
  * */
 
 @WebServlet("/admin/update_user")
-public class UpdateUserServlet extends BaseServlet {
+public class UpdateUserServlet extends HttpServlet {
 	private static final long serialVersionUID = 1L;
        
    
@@ -27,7 +27,7 @@ public class UpdateUserServlet extends BaseServlet {
 
 	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 	
-		UserServices userServices = new UserServices(entityManager, request, response);
+		UserServices userServices = new UserServices(request, response);
 		userServices.updateUser();
 	}
 
