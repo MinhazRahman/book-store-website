5ac07d6d5806bd0995c17dcd943d871b5ca09f6f
Mohammed Rahman <minhaz357@gmail.com>
2022-08-23 20:34:47 -0400

    Implement checkLogin(email, password) method
    	- Returns the Customer object or returns null

--- src/com/bookstore/dao/CustomerDAO.java
+++ src/com/bookstore/dao/CustomerDAO.java
@@ -1,7 +1,9 @@
 package com.bookstore.dao;
 
 import java.util.Date;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 import com.bookstore.entity.Customer;
 
@@ -39,6 +41,20 @@ public class CustomerDAO extends JpaDAO<Customer> implements GenericDAO<Customer
 		
 		if(!result.isEmpty()) {
 			return result.get(0);
+		}
+		
+		return null;
+	}
+	
+	public Customer checkLogin(String email, String password) {
+		Map<String, Object> parameters = new HashMap<>();
+		parameters.put("email", email);
+		parameters.put("password", password);
+		
+		List<Customer> customerList = super.findWithNamedQuery("Customer.checkLogin", parameters);
+
+		if(!customerList.isEmpty()) {
+			return customerList.get(0);
 		}
 		
 		return null;
